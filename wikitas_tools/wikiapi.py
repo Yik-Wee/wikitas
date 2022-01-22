3dbc442e3429d87977a31d76630ff699c6876e97
Yik Wee <yikweekoh@gmail.com>
2022-01-22 12:15:44 +0800

    Update docs, remove useless for loop

--- wikitas_tools/wikiapi.py
+++ wikitas_tools/wikiapi.py
@@ -1,7 +1,7 @@
 """
 Functions to interface with the wikipedia api
 """
-from typing import List, Dict
+from typing import List, Dict, Any
 from concurrent.futures import ThreadPoolExecutor
 import requests
 from .tree import Tree
@@ -11,7 +11,7 @@ MAX_THREADS = 32
 session = requests.Session()
 session.mount(
     'https://',
-    requests.adapters.HTTPAdapter(pool_maxsize=MAX_THREADS)  # max 8 threads
+    requests.adapters.HTTPAdapter(pool_maxsize=MAX_THREADS)
 )
 URL = "https://en.wikipedia.org/w/api.php"
 PARAMS = {
@@ -34,17 +34,22 @@ PARAMS_CATEGORIES = {
 
 
 class TitleNotFoundError(Exception):
+    """
+    Raised when Wikipedia title is not found and has no close matches
+    """
+
     def __init__(self, title: str, res: list) -> None:
-        super().__init__(f"Title '{title}' is not a valid Wikipedia page.\nWikipedia response: {res}")
+        super().__init__(
+            f"Title '{title}' is not a valid Wikipedia page.\nWikipedia response: {res}"
+        )
 
 
 def wikititle(title: str) -> str:
     """
     Convert `title` to existing wikipedia title using wikimedia's opensearch
     https://en.wikipedia.org/w/api.php?action=opensearch&search={title}&limit=max&namespace=0&format=json
-
     e.g. wikititle('among us') -> 'Among_Us'
-    
+
     Raises:
     ------
         `TitleNotFoundError` - if there are no close matches to the invalid wikipedia `title`
@@ -54,9 +59,7 @@ def wikititle(title: str) -> str:
         "search": title,
     }
     res = session.get(URL, params=params).json()
-    # e.g. 'among us' -> [
-    #   'Among Us', 'Among Us Hide...', 'They Are Among Us', ...
-    # ]
+    # e.g. 'among us' -> ['Among Us', 'Among Us Hide...', 'They Are Among Us', ...]
     possible_results = res[1]
     if not possible_results:
         raise TitleNotFoundError(title, res)
@@ -65,8 +68,9 @@ def wikititle(title: str) -> str:
 
 def get_links(title: str) -> List[str]:
     """
-    Get all links with namespace=0 from wikipedia page `title` from
+    Get all links with namespace=0 from valid wikipedia page `title` from
     https://en.wikipedia.org/w/api.php?action=query&format=json&titles={title}&prop=links&pllimit=max
+    Returns empty list for invalid titles
     """
     params = {
         **PARAMS,
@@ -94,6 +98,7 @@ def get_categories(title: str) -> List[str]:
     """
     Gets the categories of the wikipedia page `title` from
     https://en.wikipedia.org/w/api.php?action=query&format=json&titles={title}&prop=categories
+    Returns empty list for invalid title with no redirects
     """
     params = {
         **PARAMS_CATEGORIES,
@@ -121,16 +126,22 @@ def get_categories(title: str) -> List[str]:
 
 def _get_links_with_page(page: Tree) -> List[str]:
     """
-    Helper for `get_links_parallel`.
-    Returns page and links as a tuple:
-        (page, links)
+    Helper for `get_links_parallel`
+    Returns page and links as a dict:
+        {
+            "page": {page: Tree},
+            "links: [...],
+        }
     """
-    return (page, get_links(page.root))
+    return {
+        "page": page,
+        "links": get_links(page.root)
+    }
 
 
-def get_links_parallel(pages: List[Tree]) -> List[Dict]:
+def get_links_parallel(pages: List[Tree]) -> List[Dict[str, Any]]:
     """
-    Get links from `pages` using parallel threads to make http reqs.
+    Get links from `pages` using parallel threads to make http reqs
     Returns a list in the form of:
         [
             {
@@ -140,15 +151,8 @@ def get_links_parallel(pages: List[Tree]) -> List[Dict]:
             ...
         ]
     """
-    # titles_links = defaultdict(list)
     links_pages_list = []
-
     with ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
-        for page, links in executor.map(_get_links_with_page, pages):
-            # create page-links obj and append to links
-            links_pages_list.append({
-                "page": page,
-                "links": links,
-            })
+        links_pages_list = list(executor.map(_get_links_with_page, pages))
 
     return links_pages_list
